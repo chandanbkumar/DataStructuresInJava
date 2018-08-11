abfff1e92732169f81e83e0b867e21dfbedff2e7
chandanbkumar <chandanbinaykumar@gmail.com>
2018-08-11 20:29:09 +0530

    single LL insert delete completed

--- DSA/src/LinkedList/LinkedListDriver.java
+++ DSA/src/LinkedList/LinkedListDriver.java
@@ -4,9 +4,18 @@ public class LinkedListDriver {
 
 	public static void main(String[] args) {
 		SingleLinkedList l = new SingleLinkedList();
-		l.addFirst(52);l.addFirst(47);
+		l.addLast(1);l.addFirst(2);
+		l.add(3,l.length());
+		l.deleteFirst();
 		l.displayList();
-
+		System.out.println(l.length());
+		l.addLast(4);
+		l.addFirst(5);l.addFirst(6);
+		l.addLast(7);l.addLast(8);
+		l.addFirst(9);l.displayList();
+		System.out.println("se"+l.search(4));System.out.println(l.length());
+		l.delete(9);
+		l.displayList();
 	}
 
 }
--- DSA/src/LinkedList/SingleLinkedList.java
+++ DSA/src/LinkedList/SingleLinkedList.java
@@ -28,14 +28,88 @@ public class SingleLinkedList{
     		head.data=data;
     	}
     }
+    
+    public void addLast(int data){
+    	Node i;
+    	for(i=head;i.next!=null;i=i.next);
+    	i.next=new Node();
+    	i.data=data;
+    }
+    
     public void displayList(){
+    	System.out.println(head);
     	for(Node i=head;i.next!=null;i=i.next){
-    		System.out.println(i.data);
+    		System.out.print(i.data+" --> ");
     	}
     }
     
+    public void add(int data,int position){
+    	int len=length(),ins=0;
+    	Node i,j=null;
+    	if(position>len+1 || position<=0)  return;
+    	else if(position==1) addFirst(data);
+    	else if(position==len+1) addLast(data);
+    	else{
+    		for(i=head;ins!=position-1;i=i.next){
+    			ins++;
+    			j=i;
+    		}
+    		j.next=new Node();
+    		j.next.data=data;
+    		j.next.next=i;
+    	}
+    }
     
+    public int length(){
+    	int count=0;
+    	for(Node i=head;i.next!=null;i=i.next) count++;
+    	return count;
+    }
     
+    public int search(int data){
+    	Node i;
+    	int position=0;
+    	
+    	for(i=head;i.data!=data;i=i.next){
+    		position++;
+    	}
+    	if(position==length()) return -1;
+    	return position+1;
+    }
     
+    public void deleteFirst(){
+    	if(head.next==null) return;
+    	else{
+    		Node i=head;
+    		head=head.next;
+    		i.next=null;
+    		i=null;
+    	}
+    }
+    
+    public void deleteLast(){
+    	Node i,j=null;
+    	for(i=head;i.next!=null;i=i.next){
+    		j=i;
+    	}
+    	j.next=null;
+    	i=null;
+    }
 
+    public void delete(int data){
+    	int pos = search(data),c=1;
+    	Node i,j=null;
+    	if(pos<0) return;
+    	if(pos==1) deleteFirst();
+    	else if(pos==length()) deleteLast();
+    	else{
+    		for(i=head;c<pos;i=i.next){
+    			j=i;c++;
+    		}
+    		j.next=i.next;
+    		i.next=null;
+    		i=null;
+    	}
+    	
+    }
 }
